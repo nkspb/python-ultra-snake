52de6eff262a57cbce33c98bfc1354f4126b79f5
Nikolay Komolov <nkspb13@gmail.com>
2018-05-14 21:07:43 +0300

    Add movement logics

--- app.py
+++ app.py
@@ -5,14 +5,30 @@ import time
 import sys
 
 # Initialize pygame
-pygame.init()
+check_errors = pygame.init()
 
+# Second value of tuple is the num of errors
+if check_errors[1] > 0:
+    print('(!) Had {} initialing errors, exiting...'.format(check_errors[1]))
+    sys.exit(-1)
+else:
+    print('(+) PyGame initialized successfully!')
 # Create window
 surface = pygame.display.set_mode((640, 480))
 pygame.display.set_caption('Ultra Snake')
 
 # Define important variables
 white = pygame.Color(255, 255, 255)
+
+# Snake position
+snake_pos = [100, 50]
+snake_body = [[100, 50], [90, 50], [80, 50]]
+
+direction = 'RIGHT'
+change_to = direction
+
+score = 0
+
 # Game window size
 W_SIZE = 640
 H_SIZE = 480
@@ -81,6 +97,38 @@ while True:
                 # Create an event
                 # pygame.event.post(pygame.event.Event(pygame.QUIT))
                 main_menu.enable()
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                change_to = 'RIGHT'
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                change_to = 'LEFT'
+            if event.key == pygame.K_UP or event.key == ord('w'):
+                change_to = 'UP'
+            if event.key == pygame.K_DOWN or event.key == ord('s'):
+                change_to = 'DOWN'
+
+    # Direction validation
+    if change_to == 'RIGHT' and not direction == 'LEFT':
+        direction = 'RIGHT'
+    if change_to == 'LEFT' and not direction == 'RIGHT':
+        direction = 'LEFT'
+    if change_to == 'UP' and not direction == 'DOWN':
+        direction = 'UP'
+    if change_to == 'DOWN' and not direction == 'UP':
+        direction = 'DOWN'
+
+    # Change position
+    if direction == 'RIGHT':
+        snake_pos[0] += 10
+    if direction == 'LEFT':
+        snake_pos[0] -= 10
+    if direction == 'UP':
+        snake_pos[1] -= 10
+    if direction == 'DOWN':
+        snake_pos[1] += 10
+
+    
+    
+
 
     # Update display
     main_menu.mainloop(events)
